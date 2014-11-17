72b1059de82194333a39b5a2e1bc6bd3c37a5cf8
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2014-11-17 17:45:57 +0000

    Super basic models.

---
+++ app/src/main/java/com/team1_k/project/seg/dataviz/model/Country.java
@@ -0,0 +1,10 @@
+package com.team1_k.project.seg.dataviz.model;
+
+/**
+ * Created by alexstoick on 11/17/14.
+ */
+public class Country {
+
+    String name ;
+    Metric[] metrics ;
+}
---
+++ app/src/main/java/com/team1_k/project/seg/dataviz/model/Metric.java
@@ -0,0 +1,14 @@
+package com.team1_k.project.seg.dataviz.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by alexstoick on 11/17/14.
+ */
+public class Metric {
+
+    String keyword;
+    HashMap<Integer,Integer> dataPoints;
+
+}
