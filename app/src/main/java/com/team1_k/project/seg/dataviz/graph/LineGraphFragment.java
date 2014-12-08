968a36b46f8ade0fc7ad46d920b9c8964bae99b8
dbrisingr <phoenix.feather95@gmail.com>
2014-12-08 11:22:36 +0000

    Wot?
    asdadsa

--- app/src/main/java/com/team1_k/project/seg/dataviz/graph/LineGraphFragment.java
+++ app/src/main/java/com/team1_k/project/seg/dataviz/graph/LineGraphFragment.java
@@ -1,13 +1,23 @@
 package com.team1_k.project.seg.dataviz.graph;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
+import android.view.*;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarLineChartBase.BorderPosition;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.OnChartGestureListener;
+import com.github.mikephil.charting.interfaces.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.Legend;
+import com.github.mikephil.charting.utils.LimitLine;
 import com.team1_k.project.seg.dataviz.R;
 import com.team1_k.project.seg.dataviz.model.DataPoint;
 
@@ -17,110 +27,269 @@ import java.util.List;
 /**
  * Created by alexstoick on 12/6/14.
  */
-public class LineGraphFragment extends Fragment {
-    public void updateData(ArrayList<ArrayList<DataPoint>> dataPointsArray) {}
-/*
-    private static final String LOG_TAG = "ui.graph.line_graph.fragment";
+public class LineGraphFragment extends Fragment implements OnSeekBarChangeListener,
+        OnChartGestureListener, OnChartValueSelectedListener {
 
-    LineChartView chart;
-    LineChartData data;
+    private LineChart mChart;
+    private SeekBar mSeekBarX, mSeekBarY;
+    private TextView tvX, tvY;
+    private View rootView;
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        rootView = inflater.inflate(R.layout.fragment_line_chart, container, false);
+
+
+        getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        tvX = (TextView) rootView.findViewById(R.id.tvXMax);
+        tvY = (TextView) rootView.findViewById(R.id.tvYMax);
+
+        mSeekBarX = (SeekBar) rootView.findViewById(R.id.seekBar1);
+        mSeekBarY = (SeekBar) rootView.findViewById(R.id.seekBar2);
+
+        mSeekBarX.setProgress(45);
+        mSeekBarY.setProgress(100);
+
+        mSeekBarY.setOnSeekBarChangeListener(this);
+        mSeekBarX.setOnSeekBarChangeListener(this);
+
+        mChart = (LineChart) rootView.findViewById(R.id.chart1);
+        mChart.setOnChartGestureListener(this);
+        mChart.setOnChartValueSelectedListener(this);
+
+        mChart.setUnit(" $");
+        mChart.setDrawUnitsInChart(true);
+
+        // if enabled, the chart will always start at zero on the y-axis
+        mChart.setStartAtZero(false);
+
+        // disable the drawing of values into the chart
+        mChart.setDrawYValues(false);
+
+        mChart.setDrawBorder(true);
+        mChart.setBorderPositions(new BorderPosition[] {
+                BorderPosition.BOTTOM
+        });
+        // no description text
+        mChart.setDescription("");
+        mChart.setNoDataTextDescription("You need to provide data for the chart.");
+
+        // // enable / disable grid lines
+        // mChart.setDrawVerticalGrid(false);
+        // mChart.setDrawHorizontalGrid(false);
+        //
+        // // enable / disable grid background
+        mChart.setDrawGridBackground(true);
+        mChart.setBackgroundColor(Color.WHITE);
+        mChart.setValueTextColor(Color.RED);
+        //
+        // mChart.setDrawXLegend(false);
+        // mChart.setDrawYLegend(false);
+
+        // enable value highlighting
+        mChart.setHighlightEnabled(true);
+
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        mChart.setPinchZoom(true);
+
+
+        // create a custom MarkerView (extend MarkerView) and specify the layout
+        // to use for it
+       // MyMarkerView mv = new MyMarkerView(this, R.layout.custom_marker_view);
+
+        // define an offset to change the original position of the marker
+        // (optional)
+        //mv.setOffsets(-mv.getMeasuredWidth() / 2, -mv.getMeasuredHeight());
+
+        // set the marker to the chart
+        //mChart.setMarkerView(mv);
+
+        // enable/disable highlight indicators (the lines that indicate the
+        // highlighted Entry)
+        mChart.setHighlightIndicatorEnabled(false);
+
+        // add data
+        setData(45, 100);
+
+        mChart.animateX(2500);
+
+//        // restrain the maximum scale-out factor
+//        mChart.setScaleMinima(3f, 3f);
+//
+//        // center the view to a specific position inside the chart
+//        mChart.centerViewPort(10, 50);
+
+        // get the legend (only possible after setting data)
+        Legend l = mChart.getLegend();
+
+        // modify the legend ...
+        // l.setPosition(LegendPosition.LEFT_OF_CHART);
+        l.setForm(Legend.LegendForm.LINE);
+
+        // // dont forget to refresh the drawing
+        // mChart.invalidate();
+
+        return rootView;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        tvX.setText("" + (mSeekBarX.getProgress() + 1));
+        tvY.setText("" + (mSeekBarY.getProgress()));
+
+        setData(mSeekBarX.getProgress() + 1, mSeekBarY.getProgress());
+
+        // redraw
+        mChart.invalidate();
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+
+    }
+
     int numberOfLines ;
     int numberOfPoints ;
 
     float[][] randomNumbersTab;
 
-    boolean hasAxes = true;
-    boolean hasAxesNames = true;
-    boolean hasLines = true;
-    boolean hasPoints = true;
-    ValueShape shape = ValueShape.CIRCLE;
-    boolean isFilled = false;
-    boolean hasLabels = true;
-    boolean isCubic = false;
-    boolean hasLabelForSelected = false;
 
-    public LineGraphFragment() {
+    private void setData(int count, float range) {
+
+        //x values to change
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < count; i++) {
+            xVals.add((i) + "");
+        }
+
+        /**
+         *
+         */
+//        numberOfLines = dataPointsArray.size() ;
+//        numberOfPoints = 60 ;
+//        randomNumbersTab = new float[numberOfLines][numberOfPoints];
+//        for ( int i = 0 ; i < numberOfLines ; ++ i ) {
+//            ArrayList<DataPoint> dataPoints = dataPointsArray.get(i);
+//            numberOfPoints = dataPoints.size();
+//            for (int j = 0; j < numberOfPoints; ++j) {
+//                DataPoint currentDataPoint = dataPoints.get(j);
+//                randomNumbersTab[i][j] =
+//                        (float) currentDataPoint.getValue();
+//            }
+//        }
+//        generateData();
+
+        /**
+         *
+         */
+        //y values to change
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+        for (int i = 0; i < count; i++) {
+            float mult = (range + 1);
+            float val = (float) (Math.random() * mult) + 3;// + (float)
+            // ((mult *
+            // 0.1) / 10);
+            yVals.add(new Entry(val, i));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(yVals, "DataSet 1");
+        // set1.setFillAlpha(110);
+        // set1.setFillColor(Color.RED);
+
+        // set the line to be drawn like this "- - - - - -"
+        set1.enableDashedLine(10f, 5f, 0f);
+        set1.setColor(Color.BLACK);
+        set1.setCircleColor(Color.BLACK);
+        set1.setLineWidth(1f);
+        set1.setCircleSize(4f);
+        set1.setFillAlpha(65);
+        set1.setFillColor(Color.BLACK);
+        // set1.setShader(new LinearGradient(0, 0, 0, mChart.getHeight(),
+        // Color.BLACK, Color.WHITE, Shader.TileMode.MIRROR));
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+        dataSets.add(set1); // add the datasets
+
+        // create a data object with the datasets
+        LineData data = new LineData(xVals, dataSets);
+
+        LimitLine ll1 = new LimitLine(130f);
+        ll1.setLineWidth(4f);
+        ll1.enableDashedLine(10f, 10f, 0f);
+        ll1.setDrawValue(true);
+        ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT);
+
+        LimitLine ll2 = new LimitLine(-30f);
+        ll2.setLineWidth(4f);
+        ll2.enableDashedLine(10f, 10f, 0f);
+        ll2.setDrawValue(true);
+        ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT);
+
+        data.addLimitLine(ll1);
+        data.addLimitLine(ll2);
+
+        // set data
+        mChart.setData(data);
+    }
+
+    @Override
+    public void onChartLongPressed(MotionEvent me) {
+        Log.i("LongPress", "Chart longpressed.");
+    }
+
+    @Override
+    public void onChartDoubleTapped(MotionEvent me) {
+        Log.i("DoubleTap", "Chart double-tapped.");
+    }
+
+    @Override
+    public void onChartSingleTapped(MotionEvent me) {
+        Log.i("SingleTap", "Chart single-tapped.");
+    }
+
+    @Override
+    public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+        Log.i("Fling", "Chart flinged. VeloX: " + velocityX + ", VeloY: " + velocityY);
+    }
+
+    @Override
+    public void onValueSelected(Entry e, int dataSetIndex) {
+        Log.i("Entry selected", e.toString());
+    }
+
+    @Override
+    public void onNothingSelected() {
+        Log.i("Nothing selected", "Nothing selected.");
     }
 
     public void updateData(ArrayList<ArrayList<DataPoint>> dataPointsArray) {
-        numberOfLines = dataPointsArray.size() ;
-        numberOfPoints = 60 ;
-        randomNumbersTab = new float[numberOfLines][numberOfPoints];
-        for ( int i = 0 ; i < numberOfLines ; ++ i ) {
-            ArrayList<DataPoint> dataPoints = dataPointsArray.get(i);
-            numberOfPoints = dataPoints.size();
-            for (int j = 0; j < numberOfPoints; ++j) {
-                DataPoint currentDataPoint = dataPoints.get(j);
-                randomNumbersTab[i][j] =
-                        (float) currentDataPoint.getValue();
-            }
-        }
+        //this.dataPointsArray = dataPointsArray;
+        setData(50, 400);
+
         generateData();
     }
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public void generateData(){
 
-        View rootView = inflater.inflate(R.layout.fragment_line_chart, container, false);
-
-        chart = (LineChartView) rootView.findViewById(R.id.chart);
-        chart.setOnValueTouchListener(new ValueTouchListener());
-        chart.setInteractive(true);
-        return rootView;
     }
 
-    private void generateData() {
-
-        List<Line> lines = new ArrayList<Line>();
-        for (int i = 0; i < numberOfLines; ++i) {
-
-            List<PointValue> values = new ArrayList<PointValue>();
-            for (int j = 0; j < numberOfPoints; ++j) {
-                values.add(new PointValue(j, randomNumbersTab[i][j]));
-                Log.i(LOG_TAG, String.valueOf(randomNumbersTab[i][j]));
-            }
-
-            Line line = new Line(values);
-            line.setColor(Utils.COLORS[i]);
-            line.setShape(shape);
-            line.setCubic(isCubic);
-            line.setFilled(isFilled);
-            line.setHasLabels(hasLabels);
-            line.setHasLabelsOnlyForSelected(hasLabelForSelected);
-            line.setHasLines(hasLines);
-            line.setHasPoints(hasPoints);
-            lines.add(line);
-        }
-
-        data = new LineChartData(lines);
-
-        if (hasAxes) {
-            Axis axisX = new Axis();
-            Axis axisY = new Axis().setHasLines(true);
-            if (hasAxesNames) {
-                axisX.setName("Year");
-                axisY.setName("GDP");
-            }
-            data.setAxisXBottom(axisX);
-            data.setAxisYLeft(axisY);
-        } else {
-            data.setAxisXBottom(null);
-            data.setAxisYLeft(null);
-        }
-
-        data.setBaseValue(Float.NEGATIVE_INFINITY);
-        chart.setLineChartData(data);
-    }
-
-
-    private class ValueTouchListener implements LineChartView.LineChartOnValueTouchListener {
-
-        @Override
-        public void onValueTouched(int selectedLine, int selectedValue, PointValue value) {
-            Toast.makeText(getActivity(), "Selected: " + value, Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onNothingTouched() {
-        }
-    }*/
 }
