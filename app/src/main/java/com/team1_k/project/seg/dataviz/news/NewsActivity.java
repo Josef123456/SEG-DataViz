d83eb3a1b7a3e101c2218d7ccdc7ce93e8fe8aa0
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2014-12-08 18:22:24 +0000

    moved file

---
+++ app/src/main/java/com/team1_k/project/seg/dataviz/news/NewsActivity.java
@@ -0,0 +1,86 @@
+package com.team1_k.project.seg.dataviz.news;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.team1_k.project.seg.dataviz.exchange_rate.ExchangeRatesActivity;
+import com.team1_k.project.seg.dataviz.activities.base.MainViewActivity;
+import com.team1_k.project.seg.dataviz.R;
+import com.team1_k.project.seg.dataviz.activities.comparison.ComparisonActivity;
+import com.team1_k.project.seg.dataviz.activities.country.CountrySelectionActivity;
+
+
+/**
+ * Activity to show RSS Feed of News (possibly from Bloomberg)
+ */
+
+public class NewsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+
+        RssFragment trial = new RssFragment();
+        getFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, trial).commit();
+
+    }
+
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_view, menu);
+        return true;
+    }
+
+    /**
+     * Inside the menu the user can easily change the activities by selecting the menu items.
+     * There are five cases inside the switch statement.
+     * The user can go to the main page (home), to see the news, exchange rate, countries and comparing the countries.
+     *
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.action_home:
+                Intent intentHome = new Intent(NewsActivity.this, MainViewActivity.class);
+                this.startActivity(intentHome);
+                break;
+            case R.id.action_News:
+                Intent intentNews = new Intent(NewsActivity.this, NewsActivity.class);
+                this.startActivity(intentNews);
+                break;
+            case R.id.action_Markets:
+                Intent intentMarkets = new Intent(NewsActivity.this, ExchangeRatesActivity.class);
+                this.startActivity(intentMarkets);
+                break;
+            case R.id.action_Countries:
+                Intent intentCountries = new Intent(NewsActivity.this, CountrySelectionActivity.class);
+                this.startActivity(intentCountries);
+                break;
+            case R.id.action_More:
+                Intent intentMore = new Intent(NewsActivity.this, ComparisonActivity.class);
+                this.startActivity(intentMore);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
+
+
+}
